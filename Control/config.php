1f1b59f9bc6ba4dd9ff146e3400eae6610699a47
Gabriel <gabrielssc.ti@gmail.com>
2019-12-17 08:25:21 -0300

    Delete config.php

--- Control/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-    define('DB_HOSTNAME', 'localhost');
-    define('DB_USERNAME', 'guildsmac');
-    define('DB_PASSWORD', 'MgS_1998');
-    define('DB_DATABASE', 'cadeirasdb');
-    define('DB_PREFIX', null);
-    define('DB_CHARSET', 'utf8');
-?>
